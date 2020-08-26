fa785041f7be454f6a0802c0f7121dfc86e97f21
Alvin Eizner <alvine137@gmail.com>
2020-08-25 21:04:21 -0400

    Update finhub.py

--- finhub.py
+++ finhub.py
@@ -7,30 +7,10 @@ import pandas as pd
 # Setup client
 finnhub_client = finnhub.Client(api_key="bsuto6n48v6qu589ijp0")
 
-#r = requests.get('https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1572651390&to=1572910590&token=bsuto6n48v6qu589ijp0')
-#print(r.json())
-# Stock candles
-#print(res)
-#print(finnhub_client.company_peers('AAPL'))
-
-#print(time.time())
-
 ticker = input("Please enter the ticker of the stock you want to check: ")
 res = finnhub_client.stock_candles(ticker.upper(), 'D', 1595557674, int(time.time())) #comparing july 23 to now
 
-#date_counter = 0
-#count = 0
-'''def get_date(message):
-    for k, v in res.items():#for loop gets current date
-        if k == 't':
-            for i in v:
-                if date_counter == count:
-                    print(message + datetime.datetime.fromtimestamp(i + 28800).strftime('%Y-%m-%d'))
-                date_counter += 1
-            date_counter = 0'''
-
 def check_ws():
-    #print(pd.DataFrame(res))
     count = 0
     ws_count = 0
     counter = 0
@@ -43,24 +23,18 @@ def check_ws():
             for item in val:
                 if count == 0:
                     prev_price = item
-                #print(f'prev: {prev_price}, current: {item}')
                 if len(downtrend_arr) < 3:
                     downtrend_arr.append(item-prev_price)
                 if len(downtrend_arr) == 3:
                     average = sum(downtrend_arr)/len(downtrend_arr)
-                    #print('downtrend: ', downtrend_arr)
-                    #print('avg: ', average)
                     if average < 0:
                         downtrend = True
                     else:
-                        #del downtrend_arr[:]
                         downtrend_arr.pop(0)
                 for k, v in res.items():#for loop checks if green candle
                     if k == 'o':
                         for i in v:
                             if counter == count:
-                                #print('open: ', i)
-                                #print('close ', item)
                                 if i < item: #checks if open < close
                                     if ws_count == 0:
                                         ws_count = 1
@@ -77,14 +51,10 @@ def check_ws():
                     ws_count += 1 
                 elif item <= prev_price and count != 0 and ws_count != 1 and downtrend == True:
                     ws_count = 0
-                    #print(f'ws: {ws_count}, prev: {prev_price} / curr: {item}')
                 if ws_count == 3:
-                    #print('ws and', downtrend_arr)
                     downtrend = False
-                    #del downtrend_arr[:]
                     for num in range(1,len(downtrend_arr)):
                         downtrend_arr.pop(0)
-                    #print(f'white soldier at close of ${item}')
                     for k, v in res.items():#for loop gets current date
                         if k == 't':
                             for i in v:
@@ -92,17 +62,12 @@ def check_ws():
                                     print('white soldier on date ' + datetime.datetime.fromtimestamp(i + 28800).strftime('%Y-%m-%d'))
                                 date_counter += 1
                             date_counter = 0
-                    #get_date('white soldier on date ')
                     ws_count = 0
                 prev_price = item
-                #print(downtrend_arr)
-                #print(item)
                 count += 1
-            #print('val: ', val)
     ws_count = 0
 
 def check_bearish_engulfing():
-    #print(pd.DataFrame(res))
     count = 0
     counter = 0
     date_counter = 0
@@ -116,32 +81,21 @@ def check_bearish_engulfing():
                     prev_price = item
                 if len(uptrend_arr) < 4:
                     uptrend_arr.append(item-prev_price)
-                    #print(f'curr: {item}, prev: {prev_price}')
-                    #print(uptrend_arr)
                 if len(uptrend_arr) == 4:
-                    #print('test')
                     average = sum(uptrend_arr)/len(uptrend_arr)
-                    #print('downtrend: ', downtrend_arr)
-                    #print('avg: ', average)
                     if average > 0:
                         uptrend = True
-                        #print('uptrend', item)
                     else:
-                        #del uptrend_arr[:]
                         uptrend_arr.pop(0)
                         uptrend = False
                 for k, v in res.items():#for loop checks if green candle
                     if k == 'o':
                         for i in v:
                             if counter == count:
-                                #print('open: ', i)
-                                #print('close ', item)
                                 if i < item and uptrend == True: #checks if open < close
                                     del engulf_arr[:]
                                     engulf_arr.append(i)#open
                                     engulf_arr.append(item)#close
-                                    #print(engulf_arr)
-                                    #print('green candle', item)
                                 elif len(engulf_arr) == 2 and i > item and uptrend == True:
                                     if i >= engulf_arr[1] and item < engulf_arr[0]:
                                         for k, v in res.items():#for loop gets current time
@@ -151,7 +105,6 @@ def check_bearish_engulfing():
                                                         print('bearish engulfing at date: ' + datetime.datetime.fromtimestamp(i + 28800).strftime('%Y-%m-%d'))
                                                     date_counter += 1
                                                 date_counter = 0
-                                        #print(f'bearish engulfing at ${item} and average = {average} and arr is {uptrend_arr}')
                                         uptrend_arr.pop(0)
                                         uptrend_arr.append(item-prev_price)
                                     else:
@@ -161,17 +114,13 @@ def check_bearish_engulfing():
                                 elif i >= item and uptrend == True and len(engulf_arr) != 2:
                                     uptrend_arr.pop(0)
                                     uptrend = False
-                                    #green_candle = False
                                     del engulf_arr[:]
                                 else:
                                     del engulf_arr[:]
                             counter += 1
                         counter = 0
-                #print(uptrend_arr)
                 prev_price = item
-                #print(item)
                 count += 1
-            #print('val: ', val)
 
 def check_bullish_engulfing():
     count = 0
@@ -226,10 +175,7 @@ def check_bullish_engulfing():
                             counter += 1
                         counter = 0
                 prev_price = item
-                #print(item)
                 count += 1
-            #print('val: ', val)
-
 
 check_ws()
 check_bearish_engulfing()
